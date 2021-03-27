790db21bfde7757cad810e9ab138a2f792c99b95
Robert DeRouin <robertjosephderouin@gmail.com>
2021-03-27 15:10:24 -0700

    Add variables for index's table

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,12 @@
 //Variables
 const welcome = document.getElementById('welcome');
+const userInfo = document.getElementById('userInfo');
+const userName = document.getElementById('userName');
+const userAddress = document.getElementById('userAddress');
+const userEmail = document.getElementById('userEmail');
+const userStride = document.getElementById('userStride');
+const userGoal = document.getElementById('userGoal');
+const userFriends = document.getElementById('userFriends');
 
 const usersArray = userData.map(user => new User(user))
 const repository = new UserRepository(usersArray);
@@ -7,10 +14,12 @@ let displayUser = repository.getUserData(1);
 
 //EventListeners
 window.addEventListener('DOMContentLoaded', (event) => {
-  welcome.innerText = `Welcome ${displayUser.getFirstName()}!`
+  renderUser();
 });
 
 //Functions
 
-
+function renderUser() {
+  welcome.innerText = `Welcome ${displayUser.getFirstName()}!`
+}
 
