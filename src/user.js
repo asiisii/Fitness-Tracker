a1cc3b2d6ea7d3c3360154764d69ce8e47cba870
Robert DeRouin <robertjosephderouin@gmail.com>
2021-03-31 15:39:30 -0700

    Refactor iteration 3 function into 1 function rather than multiple.

--- src/user.js
+++ src/user.js
@@ -13,35 +13,32 @@ class User {
     return this.name.split(' ')[0];
   }
 
-  getAverageFluidOunces(hydrationData, date) {
-    let hydration = hydrationData.filter(data => data.userID === this.id);
+  getAverageData (dataArray, targetData, date) {
+    let filteredArray = dataArray.filter(data => data.userID === this.id);
     if (typeof date === "string") {
-      hydration = hydration.filter(data => data.date === date);
-      if (!hydration.length) {
+      filteredArray = filteredArray.filter(data => data.date === date);
+      if (!filteredArray.length) {
         return 0;
       }
     } else if (date instanceof Array) {
       // Some see if ANY thing in the array see if the data equals the data date.
-      hydration = hydration.filter(data => date.some(date => data.date === date));
-      if (!hydration.length) {
+      filteredArray = filteredArray.filter(data => date.some(date => data.date === date));
+      if (!filteredArray.length) {
         return 0;
       }
     }
-    return hydration.map(data => data.numOunces).reduce((total, current) => total + current) / hydration.length;
+    return filteredArray.map(data => data[targetData]).reduce((total, current) => total + current) / filteredArray.length;
   }
 
-  getAvgSleepInfo(sleepData, sleepType) {
-    let userSleepData = sleepData.filter(user => user.userID === this.id);
-    let sleptHrs = userSleepData.map(hour => hour[sleepType]);
-    return sleptHrs.reduce((total, time) => total += time) / userSleepData.length; 
+  getAverageFluidOunces(hydrationData, date) {
+    return this.getAverageData(hydrationData, "numOunces", date);
   }
 
-  getSleepInfoByDate(sleepData, date, sleepType) {
-    let userSleepData = sleepData.filter(user => user.userID === this.id);
-    return userSleepData.filter(day => day.date === date)[0][sleepType]
+  getSleepInfo(sleepData, sleepType, date) {
+    return this.getAverageData(sleepData, sleepType, date)
   }
 
-  findHpyersomnia(day) {
+  getHypersomnia(day) {
     let userSleepData = sleepData.filter(user => user.date === day)
     let getHrs = userSleepData.map(user => user.hoursSlept)
     // console.log('getHrs', getHrs);
@@ -50,7 +47,6 @@ class User {
     let idOfSomnia = userSleepData.filter(user => user.hoursSlept === highestHrs)[0].userID
     // console.log(idOfSomnia);
     let somniaName = userData.filter(user => user.id === idOfSomnia)[0].name
-    console.log(somniaName);
     return somniaName
   }
 
