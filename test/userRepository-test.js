de2b8f25b2298077289fedbb41894eb7a900a5ac
Ashish Malla <ashishmalla45@gmail.com>
2021-04-03 13:40:43 -0600

    Refactor test for getUserData method

--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -51,15 +51,7 @@ describe('User Repository', () => {
   })
 
   it('should return an object using user ID', () => {
-    expect(userRepository.getUserData(1)).to.deep.equal({
-      "id": 1,
-      "name": "Luisa Hane",
-      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-      "email": "Diana.Hayes1@hotmail.com",
-      "strideLength": 4.3,
-      "dailyStepGoal": 10000,
-      "friends": [ 16, 4, 8 ]
-    })
+    expect(userRepository.getUserData(1)).to.deep.equal(userData[0])
   })
 
   it('should produce an average step count', () => {
